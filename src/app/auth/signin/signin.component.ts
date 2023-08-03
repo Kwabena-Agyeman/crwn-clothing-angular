dfcbd3f533ed5383d9f4bd2ee730581f3786b436
Kwabena Agyeman <kagyeman@ymail.com>
2023-08-03 13:14:35 +0000

    set up login form component

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  AbstractControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.scss']
+  styleUrls: ['./signin.component.scss'],
 })
 export class SigninComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
 
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
 }
