a4bae462850bcb41edb64aecc312a6224ddaa203
Kwabena Agyeman <kagyeman@ymail.com>
2023-08-03 14:11:34 +0000

    Setup login with firebase

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import {
-  FormGroup,
-  FormControl,
-  Validators,
-  AbstractControl,
-} from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { FirebaseError } from '@angular/fire/app';
 
 @Component({
   selector: 'app-signin',
@@ -12,12 +11,32 @@ import {
   styleUrls: ['./signin.component.scss'],
 })
 export class SigninComponent {
+  constructor(
+    private authService: AuthService,
+    private toast: ToastrService,
+    private router: Router
+  ) {}
+
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required]),
   });
 
-  onSubmit() {
-    console.log(this.loginForm);
+  async onSubmit() {
+    if (this.loginForm.invalid) return;
+
+    try {
+      const { email, password } = this.loginForm.value;
+      await this.authService.login(email!, password!);
+      this.toast.success('Registration successful');
+      this.router.navigateByUrl('/');
+    } catch (error) {
+      console.error(error);
+      if (error instanceof FirebaseError) {
+        this.toast.warning(error.code);
+        return;
+      }
+      this.toast.error('Something went wrong');
+    }
   }
 }
