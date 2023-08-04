27622df802c9a7861808a4fef395adb0f1d9459b
Kwabena Agyeman <kagyeman@ymail.com>
2023-08-04 21:44:03 +0000

    added environment files to git gitignore

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyAN8YwZY2r3k9pgIfENurO1sxUKnt-BrBA',
+    authDomain: 'crwn-clothing-db-c0c94.firebaseapp.com',
+    projectId: 'crwn-clothing-db-c0c94',
+    storageBucket: 'crwn-clothing-db-c0c94.appspot.com',
+    messagingSenderId: '707546973288',
+    appId: '1:707546973288:web:458b4266a2f2773b35a7dd',
+    measurementId: 'G-5EBHK05N8N',
+  },
+  stripePublicKey:
+    'pk_test_51In9cWG42nDJHmmJrFgCHFyaO7IXcRBBtTmvpP5116Mx6MHu97P3Pzulg9MkQHuAGwVWIVkZEp9ZOhb2lnp3QClw00rGkfuHgK',
+};
